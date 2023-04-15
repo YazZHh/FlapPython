9318f96b6a57129fb3cb2c45ec237713a11c4139
YazZHh <amaury.devalicourt@gmail.com>
2023-04-15 10:20:54 +0400

    ajout d'un 2e floor pour continuité

--- src/floor.py
+++ src/floor.py
@@ -1,13 +1,14 @@
 import pygame
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, offset=0):
         super().__init__()
         self.image = pygame.image.load("img/terre.jpg")
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.x = 360 - self.width
+        self.x = 360 - offset
         self.y = 600 - self.height
+        self.image = pygame.transform.scale(self.image, (370, 150))
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
         self.speed = -3
         self.sprite = pygame.sprite.Group()
@@ -16,4 +17,4 @@ class Floor(pygame.sprite.Sprite):
     def move(self):
         self.rect.move_ip(self.speed, 0)
         if self.rect.right < 0:
-            self.rect.left = 360    
+            self.rect.left = 360

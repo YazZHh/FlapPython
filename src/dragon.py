1fe540b73650a27924c02d5a6314da466dac11a4
Novah974 <noah974.techer@gmail.com>
2023-04-16 11:07:07 +0400

    add detail

--- src/dragon.py
+++ src/dragon.py
@@ -5,16 +5,25 @@ class Dragon(pygame.sprite.Sprite):
     def __init__(self, Jeu):
         self.jeu = Jeu
         self.vitesse_descendante = -8
-        self.image = pygame.transform.scale(pygame.image.load("img/dragon.png"), (60, 60))
+        
+        self.image = pygame.image.load("img/dragon.png")
+        self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
         self.rect.x = 60
         self.rect.y = 100
+
+        self.image_normal = self.image
+        self.image_up = pygame.transform.rotate(self.image, 30)
+        self.image_down = pygame.transform.rotate(self.image, -20)
 
     def gravite(self):
         if self.jeu.pret:
             if self.rect.y < 460:
                 self.rect.y += self.vitesse_descendante
                 self.vitesse_descendante += 0.45
+            if self.rect.y > 280:
+                self.image = self.image_down
 
     def check_collision(self, group):
         return pygame.sprite.spritecollide(self, group, False, pygame.sprite.collide_mask)
+    
--- src/game.py
+++ src/game.py
@@ -9,12 +9,12 @@ class Game:
         self.dragon = Dragon(self)
 
 
-        self.taille_tour = random.randint(100, 350)
+        self.taille_tour = random.randint(50, 350)
         self.tour1 = Tours(self, self.taille_tour, 310, 0, False)
         self.tour2 = Tours(self, 400-self.taille_tour, 310, self.taille_tour + 150)
         self.toutes_tours1 = pygame.sprite.Group()
 
-        self.taille_tour = random.randint(100, 350)
+        self.taille_tour = random.randint(50, 350)
         self.tour3 = Tours(self, self.taille_tour, 510, 0, False)
         self.tour4 = Tours(self, 400-self.taille_tour, 510, self.taille_tour + 150)
         self.toutes_tours2 = pygame.sprite.Group()
@@ -22,17 +22,17 @@ class Game:
         self.pret = False
 
     def creation_tour(self):
-        self.toutes_tours1.add(self.tour1)
-        self.toutes_tours1.add(self.tour2)
-        self.taille_tour = random.randint(100, 350)
+        self.taille_tour = random.randint(50, 350)
         self.tour1 = Tours(self, self.taille_tour, 310, 0, False)
         self.tour2 = Tours(self, 400-self.taille_tour, 310, self.taille_tour + 150)
-
-        self.toutes_tours2.add(self.tour3)
-        self.toutes_tours2.add(self.tour4)
-        self.taille_tour = random.randint(100, 350)
+        self.toutes_tours1.add(self.tour1)
+        self.toutes_tours1.add(self.tour2)
+        
+        self.taille_tour = random.randint(150, 350)
         self.tour3 = Tours(self, self.taille_tour, 510, 0, False)
         self.tour4 = Tours(self, 400-self.taille_tour, 510, self.taille_tour + 150)
+        self.toutes_tours2.add(self.tour3)
+        self.toutes_tours2.add(self.tour4)
 
 
     def start(self):
@@ -45,22 +45,22 @@ class Game:
     def saut(self):
         if self.dragon.rect.y > 10:
             self.dragon.vitesse_descendante = -8
+            self.dragon.image = self.dragon.image_up
 
     def respawn(self, pseudo):
-        self.tour1 = Tours(self, self.taille_tour, 410, 0, False)
-        self.tour2 = Tours(self, 400-self.taille_tour, 410, self.taille_tour + 150)
         if pseudo == 1:
+            self.taille_tour = random.randint(50, 350)
+            self.tour1 = Tours(self, self.taille_tour, 410, 0, False)
+            self.tour2 = Tours(self, 400-self.taille_tour, 410, self.taille_tour + 150)
             self.toutes_tours1.add(self.tour1)
             self.toutes_tours1.add(self.tour2)
-            self.taille_tour = random.randint(100, 350)
+        else:
+            self.taille_tour = random.randint(50, 350)
             self.tour1 = Tours(self, self.taille_tour, 410, 0, False)
             self.tour2 = Tours(self, 400-self.taille_tour, 410, self.taille_tour + 150)
-        else:
             self.toutes_tours2.add(self.tour1)
             self.toutes_tours2.add(self.tour2)
-            self.taille_tour = random.randint(100, 350)
-            self.tour1 = Tours(self, self.taille_tour, 410, 0, False)
-            self.tour2 = Tours(self, 400-self.taille_tour, 410, self.taille_tour + 150)
+            
 
     def game_over(self):
         if self.dragon.rect.y >= 460 or (self.dragon.check_collision(self.toutes_tours1) and self.dragon.check_collision(self.toutes_tours1)):
@@ -70,3 +70,4 @@ class Game:
             self.dragon.vitesse_descendante = -8
             self.dragon.rect.x = 60
             self.dragon.rect.y = 100
+            self.dragon.image = self.dragon.image_normal

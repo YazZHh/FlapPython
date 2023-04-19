0b17816903aed8320b4c7745cd1a581152de5edf
Novah974 <noah974.techer@gmail.com>
2023-04-19 10:39:52 +0400

    del start lag

--- src/game.py
+++ src/game.py
@@ -11,20 +11,25 @@ class Game:
         self.toutes_tours1 = pygame.sprite.Group()
         self.toutes_tours2 = pygame.sprite.Group()
 
-        self.pret = False
-
-    def creation_tour(self):
         self.taille_tour = random.randint(50, 325)
-        self.tour1 = Tours(self, self.taille_tour, 330, 0, False)
-        self.tour2 = Tours(self, 375-self.taille_tour, 330, self.taille_tour + 175)
+        self.tour1 = Tours(self, self.taille_tour, 370, 0, False)
+        self.tour2 = Tours(self, 375-self.taille_tour, 370, self.taille_tour + 175)
         self.toutes_tours1.add(self.tour1)
         self.toutes_tours1.add(self.tour2)
         
         self.taille_tour = random.randint(50, 325)
-        self.tour3 = Tours(self, self.taille_tour, 560, 0, False)
-        self.tour4 = Tours(self, 375-self.taille_tour, 560, self.taille_tour + 175)
+        self.tour3 = Tours(self, self.taille_tour, 370, 0, False)
+        self.tour4 = Tours(self, 375-self.taille_tour, 370, self.taille_tour + 175)
         self.toutes_tours2.add(self.tour3)
         self.toutes_tours2.add(self.tour4)
+
+        self.pret = False
+
+    def creation_tour(self):
+        self.tour1.rect.x = 330
+        self.tour2.rect.x = 330
+        self.tour3.rect.x = 560
+        self.tour4.rect.x = 560
 
 
     def start(self):
@@ -56,14 +61,16 @@ class Game:
             self.tour4.rect.y = self.taille_tour + 175
             self.tour3.image = pygame.transform.scale(self.tour3.image, (80, self.taille_tour))
             self.tour4.image = pygame.transform.scale(self.tour4.image, (80, 375 - self.taille_tour))
-        self.dragon.point += 1
+        self.dragon.point += 1  
             
 
     def game_over(self):
         if self.dragon.rect.y >= 460 or self.dragon.check_collision(self.toutes_tours1) or self.dragon.check_collision(self.toutes_tours2):
             self.pret = False
-            self.toutes_tours1 = pygame.sprite.Group()
-            self.toutes_tours2 = pygame.sprite.Group()
             self.dragon.vitesse_descendante = -8
             self.dragon.reset()
             self.dragon.image = self.dragon.image_normal
+            self.tour1.rect.x = 370
+            self.tour2.rect.x = 370
+            self.tour3.rect.x = 370
+            self.tour4.rect.x = 370
